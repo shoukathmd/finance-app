2d67a5ad59ee5ca0f0719dae4b0503c1a27376b9
shoukathmd <shoukathmd@gmail.com>
2024-07-09 20:13:38 +0530

    Added sort by email data table

--- app/(dashboard)/accounts/columns.tsx
+++ app/(dashboard)/accounts/columns.tsx
@@ -1,6 +1,8 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import { ColumnDef } from "@tanstack/react-table";
+import { ArrowUpDown, MoreHorizontal } from "lucide-react";
 
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
@@ -11,8 +13,6 @@ export type Payment = {
   email: string;
 };
 
-
-
 export const columns: ColumnDef<Payment>[] = [
   {
     accessorKey: "status",
@@ -20,7 +20,17 @@ export const columns: ColumnDef<Payment>[] = [
   },
   {
     accessorKey: "email",
-    header: "Email",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Email
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
   },
   {
     accessorKey: "amount",
