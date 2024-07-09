4416dc7c119ec6a721e3a963ca5d1bea50d02a15
shoukathmd <shoukathmd@gmail.com>
2024-07-09 10:58:00 +0530

    Header section initial checkin

---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,7 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import { UserButton } from "@clerk/nextjs";
+
+export default function Home() {
+  return <UserButton afterSwitchSessionUrl="/" />;
+}
