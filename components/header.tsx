6e2344abd301802bef456ebce35f18f204ef4cd4
shoukathmd <shoukathmd@gmail.com>
2024-07-12 07:22:20 +0530

    Added filters

--- components/header.tsx
+++ components/header.tsx
@@ -4,6 +4,7 @@ import { UserButton, ClerkLoading, ClerkLoaded } from "@clerk/nextjs";
 import { WelcomeMsg } from "@/components/welcome-msg";
 import { HeaderLogo } from "@/components/header-logo";
 import { Navigation } from "./navigation";
+import { Filters } from "./filters";
 
 export const Header = () => {
   return (
@@ -22,6 +23,7 @@ export const Header = () => {
           </ClerkLoading>
         </div>
         <WelcomeMsg />
+        <Filters />
       </div>
     </header>
   );
